8c61de78393fdd93c7bbf7fcc36cdfbaf343ccf9
Torsten Uhlmann <tuhlmann@agynamix.de>
2021-04-03 16:33:52 +0200

    Adding detection of installed command.

--- pop_os_2004.sh
+++ pop_os_2004.sh
@@ -52,6 +52,15 @@ then
   add_packages "variety"
 fi
 
+# Keybase
+if ! package_installed "keybase"
+then
+  curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb
+  sudo apt install ./keybase_amd64.deb
+  run_keybase
+  rm ./keybase_amd64.deb
+fi
+
 # Docker
 if ! package_installed "docker-ce"
 then
@@ -64,3 +73,27 @@ then
   sudo usermod -aG docker ${USER}
   echo "172.17.0.1      host.docker.internal" | sudo tee -a /etc/hosts > /dev/null 2>&1
 fi
+
+if ! command_installed "docker_compose"
+then
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+if ! command_installed "java"
+then
+  echo "Install OpenJDK 8.0.265"
+  curl -s "https://get.sdkman.io" | bash
+  source $HOME/.sdkman/bin/sdkman-init.sh
+  sdk install java 8.0.265-open
+fi
+
+if ! command_installed "node"
+then
+  echo "Install NVM and then latest node"
+  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+  source $HOME/.nvm/nvm.sh
+  nvm install --lts
+fi
+
+timedatectl set-local-rtc 1 --adjust-system-clock
